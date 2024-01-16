47a30f4996a3f5c48f23d437385cab8cd931856f
minicom <3387910@naver.com>
2024-01-17 00:47:26 +0900

    Apply multiprocessing for generation speed

--- utils/captcha_generater.py
+++ utils/captcha_generater.py
@@ -3,6 +3,8 @@ import random
 import os
 from tqdm import trange
 from config_util import configGetter
+from tqdm.contrib.concurrent import process_map
+from multiprocessing import cpu_count
 
 cfg = configGetter('DATASET')
 
@@ -11,19 +13,26 @@ image = ImageCaptcha(fonts=[cfg['CAPTCHA']['FONT_DIR']])
 def randomSeqGenerator(captcha_len):
     ret = ""
     for i in range(captcha_len):
-        num = chr(random.randint(48,57))#ASCII表示数字
-        letter = chr(random.randint(97, 122))#取小写字母
-        Letter = chr(random.randint(65, 90))#取大写字母
-        s = str(random.choice([num,letter,Letter]))
+        num = chr(random.randint(48, 57))  # ASCII表示数字
+        letter = chr(random.randint(97, 122))  # 取小写字母
+        Letter = chr(random.randint(65, 90))  # 取大写字母
+        s = str(random.choice([num, letter, Letter]))
         ret += s
     return ret
-    
+
+
+def p(args):
+    captcha_len, dataset_path, i = args
+    char_seq = randomSeqGenerator(captcha_len)
+    save_path = os.path.join(dataset_path, f'{char_seq}.{i}.png')
+    image.write(char_seq, save_path)
+
+
 def captchaGenerator(dataset_path, dataset_len, captcha_len):
     os.makedirs(dataset_path, exist_ok=True)
-    for i in trange(dataset_len):
-        char_seq = randomSeqGenerator(captcha_len)
-        save_path = os.path.join(dataset_path, f'{char_seq}.{i}.png')
-        image.write(char_seq, save_path)
+    process_map(p, [[captcha_len, dataset_path, i]
+                for i in range(dataset_len)], max_workers=cpu_count(), chunksize=256)
+
 
 def generateCaptcha():
     TRAINING_DIR = cfg['TRAINING_DIR']
@@ -34,7 +43,7 @@ def generateCaptcha():
 
     captchaGenerator(TRAINING_DIR, TRAINING_DATASET_LEN, CHAR_LEN)
     captchaGenerator(TESTING_DIR, TESTING_DATASET_LEN, CHAR_LEN)
-    #captchaGenerator('./dataset/test', 20000, 4)
-    
+    # captchaGenerator('./dataset/test', 20000, 4)
+
 if __name__ == "__main__":
     generateCaptcha()
